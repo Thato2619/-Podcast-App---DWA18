f5db8f969a75bed62305a5ce6d0baf85173a151d
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-06-29 02:02:32 +0200

    add genre folder, script folder, show folder and showGenres

---
+++ podcast/src/assets/showGenres/Investigative.js
@@ -0,0 +1,39 @@
+import '../show/10716'
+import '../show/5675'
+import '../show/10539'
+import '../show/9177'
+import '../show/8890'
+import '../show/5012'
+import '../show/9054'
+import '../show/7654'
+import '../show/8256'
+import '../show/8291'
+import '../show/5276'
+import '../show/5964'
+import '../show/6465'
+import '../show/5320'
+import '../show/6451'
+import '../show/5692'
+import '../show/6430'
+
+
+export const investigativeShows = [
+    '10716',
+    '5675',
+    '10539',
+    '9177',
+    '8890',
+    '5012',
+    '9054',
+    '7654',
+    '8256',
+    '8291',
+    '5718',
+    '5276',
+    '5964',
+    '6465',
+    '5320',
+    '6451',
+    '5692',
+    '6430'
+]
